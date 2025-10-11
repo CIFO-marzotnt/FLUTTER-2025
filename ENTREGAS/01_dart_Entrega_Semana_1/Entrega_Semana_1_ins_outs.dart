5d85f7cac527cee88d98253f3ca6beed706a9a3c
marzotnt <marzotontodonati@gmail.com>
2025-10-11 22:20:52 +0200

    explicacion .tryParse y null safety

--- ENTREGAS/01_dart_Entrega_Semana_1/Entrega_Semana_1_ins_outs.dart
+++ ENTREGAS/01_dart_Entrega_Semana_1/Entrega_Semana_1_ins_outs.dart
@@ -4,11 +4,19 @@ void
 main() {
   // 01 Convierte grados Celsius a Fahrenheit
 
+  
   stdout.writeln(
-    'Puedo convertir Celsius en Fahrenheit! \nDame una temperatura en grados Celsius.',
+    '\nPuedo convertir Celsius en Fahrenheit! \nDame una temperatura en grados Celsius.\n',
   );
 
+  /* IN: input, es de tipo String y può essere null a través de "String?"*/
   String? inputCelsius = stdin.readLineSync();
+
+  /* Conversión de input in double */
+  /* Controles de seguridad: NULL SAFETY */
+  /* I. double? para incluir valores nullos */
+  /* II. tryParse para gestionar valores null o no numericos */
+  /* III. ?? '0' como ulterior seguridad en caso de valor null */
 
   double? celsius = double.tryParse(inputCelsius ?? '0');
 
@@ -16,14 +24,12 @@ main() {
 
     double fahrenheit = (celsius * 9/5) + 32 ;
 
-    stdout.writeln('\n.....calculo el resultado.....');
-    stdout.writeln('La temperatura di $celsius °C equivale a ${fahrenheit.toString()} °F.');
+    stdout.writeln('\n.....calculo el resultado.....\n');
+    stdout.writeln('La temperatura di $celsius °C equivale a ${fahrenheit.toString()} °F.\n');
 
   } else {
 
     stdout.writeln("Input non valido.");  
     
   }
-
-
 }
