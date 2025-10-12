2ab6bbc70b5176df89f8dc5f12d8b5cff3f4ff62
marzotnt <marzotontodonati@gmail.com>
2025-10-12 20:41:57 +0200

    if else: explicación y ejemplos

--- EXEs/04_ins_outs_contol_flujo/01_ins_outs.dart
+++ EXEs/04_ins_outs_contol_flujo/01_ins_outs.dart
@@ -3,18 +3,22 @@ import 'dart:io';
 void
 main() {
   /* imprimir en el terminal:
+   *
    * stdout.write('')
+   * 
    * writeln pone un salto de linia 
   */
   stdout.writeln(
     'Hola! Cual es tu nombre?',
   );
 
-  /* Leer información
+  /* leer información
    * creo una var String que puede ser null (String?)
    * 
    * el valor de la var String es el input del utente:
+   * 
    * stdin.readLineSync()
+   * 
    * permite de leer el input
   */
   String? inputNombre = stdin.readLineSync();
--- EXEs/04_ins_outs_contol_flujo/02_if_else.dart
+++ EXEs/04_ins_outs_contol_flujo/02_if_else.dart
@@ -1,3 +1,110 @@
-void main() {
-  
+import 'dart:io';
+
+void
+main() {
+
+  /* IF y IF ELSE
+  *
+  * if  (condición que quiero) {haz esto}
+  * else if   (otra condición) {haz esto}
+  * ...
+  * else if   (otra condición) {haz esto};
+  * 
+  * puedo sumar un caso final:
+  * si todas las de antes son false
+  * (no es necesario poner condiciónes)
+  *
+  * else    {haz esto}
+  */
+
+  /* OPERADORES LÓGICOS 
+  *
+  * los siguientes aceptan solo valores booleanos:
+  * &&  AND     true solo se entrambi true
+  * ||  OR      true se almeno uno è true
+  * !   NOT     convierte da true a false
+  *
+  * [??  OR      acepta null, int, double, String alla sua sx]
+  *
+  */
+
+  int? edad = 19;
+
+  if (edad >= 21) {
+    print ('Puedes entrar al club y beber alcohol');
+  } else if (edad >= 18 && edad < 21) {
+    print ('Puedes entrar al club pero no beber alcohol');
+  } else {
+    print('Eres menor de edad. No puedes acceder al club.');
+  }
+
+    /* mismo ejercicio, con entradas y salidas:
+     *
+     * stdout.writeln();
+     * 
+     * stdin.readLineSync();
+     * el input será leido como String
+     * no 
+     * 
+     * 
+    */
+
+  stdout.writeln('Bienvenido al club. \nCual es tu edad?\n');
+
+  int? inputEdad = int.tryParse(stdin.readLineSync() ?? '0');
+
+  if (inputEdad != null) {          // concatenazione di if
+    if (inputEdad >= 21) {
+        stdout.writeln('Puedes entrar y beber alcohol');
+    } else if (inputEdad >= 18 && inputEdad<21) {
+        stdout.writeln('Puedes entrar y no beber');
+    } else {
+        stdout.writeln('Eres menor y no puedes entrar');
+        }
+
+    } else {
+        stdout.writeln('Introduce un valor válido');
+    }
+
 }
+
+
+    /* si el input es introducido por el utente (y no asignado) 
+     * ese valor puede ser cualquiera (null, caracteres non numéricos):
+     * tenemos que gestionar estas posibilidades
+    */
+
+    /* conversión String to num o double y NULL SAFETY:
+     *
+     * int/oppure double/.parse()
+     * 
+     * si el input utent es null o pone caracteres non numéricos:
+     * este valor no se podrá parsear
+     * .parse devolverá un ERROR que crashea el app
+     * .parse puede solo acceptar el tipo de var indicada (num o double) 
+     * =>   en otros casos no sabe que hacer
+     * 
+    */
+
+    /*
+     * en su lugar se usa
+     * ?    +   .tryParse    
+     * 
+     * ? = indica que puede incluir valores nulos
+     * en dart por definición una variable no puede ser nula
+     * =>   creación variable int? o double? 
+     * 
+     * .tryParse 
+     * devolverá un NULL y no un ERROR sin crashear app
+     * acepta solo valores String y no String?
+    */
+
+    /*
+     * =>   tengo que sumar otra condición de seguridad:
+     * ?? '0'
+     * 
+     * de esta forma si el valore que intenta parsear es null,
+     * no me devuelve un ERROR y le asignará el valor sostitutivo
+     * 
+     */
+  
