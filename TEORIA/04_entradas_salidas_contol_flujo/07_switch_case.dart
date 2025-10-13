133a89f096ce11ae01058639d6e7468dc28fb53a
marzotnt <marzotontodonati@gmail.com>
2025-10-14 00:57:27 +0200

    added : 01 funciones basicas

--- TEORIA/04_entradas_salidas_contol_flujo/07_switch_case.dart
+++ TEORIA/04_entradas_salidas_contol_flujo/07_switch_case.dart
@@ -1,3 +1,53 @@
+import 'dart:math';
+
 void main () {
-  
+  int rnd = Random().nextInt(3);
+  print(rnd);
+
+  // Modo 1: else if
+
+  if (rnd == 0) {
+    print('Lunes');
+  } 
+  else if (rnd == 1) {
+    print('Martes');
+  } 
+  else if (rnd == 2) {
+    print('Miercoles');
+  } else {
+    print('Nada');
+  }
+
+  // Modo 2: if
+
+  if (rnd == 0) {
+    print('Lunes');
+  }
+  if (rnd == 1) {
+    print('Martes');
+  }
+  if (rnd == 2) {
+    print('Miercoles');
+  }
+  if (rnd !=1 && rnd !=0  && rnd !=3) {
+    print('Nada');
+  }
+
+  // Modo 3: switch case 
+
+  switch (rnd) {
+    case 0: print('Lunes');
+  }
+
+
+  // Otro ejercicio
+
+  String nombre = 'Andrea';
+
+  switch (nombre) {
+    case 'Jose': print('Tu eres. Jose!!!');
+    default : print('No conozco este nombre :()');
+  }
+
+
 }
---
+++ TEORIA/05_funciones/01_funciones_basicas.dart
@@ -0,0 +1,38 @@
+
+void saludar() {}     // declaracion funcion tipo void (funcion sin retorno)
+
+String saludar2() {}  // declaracion funcion vacia pero String 
+
+int sumar() {         // declaracion funcion int
+  int num1 = 10;      // funcion puede ser cualquier bloque de codico que quiero repetir/invocar
+  int num2 = 20;
+
+  return num1 + num2; 
+}
+
+int sumarConPosicion (int? numero1, int numero2) {     // funcion posicional
+  return numero1 ?? 0 + numero2;
+}
+
+int sumarConNombre({required int numero1, int? numero2, required numero3}) {    // required pone el nombre (tag)
+  return numero1 = (numero2 ?? 0);
+}
+
+/* Existen funciones
+ * con retorno
+ * sin retorno
+*/
+
+void main() {   // ya esta es una funcion y reproduce nuestro codigo // nota run | debug 
+  var mensaje = saludar();      // nb: una var puó anche essere una funzione
+  // print(mensaje);            // con print mi da errore perché no admite void
+
+  var mensaje2 = saludar2();
+  print(mensaje2);              // esto lo admite porqué es String
+
+  int resultado = sumarConPosicion(numero1, numero2)   // evoco la funzione con una var
+  print(resultado);
+
+  int resultado2 = sumarConNombre(numero1: numero1, numero3: numero3)   // mas clara, indica los argumentos con sus tags
+  print(resultado2);
+}
