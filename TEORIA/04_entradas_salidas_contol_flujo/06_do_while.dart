82ddfe38e392341daaa5d18d50ce57c80b7b9520
marzotnt <marzotontodonati@gmail.com>
2025-10-13 22:55:11 +0200

    added : do while teoria

--- TEORIA/04_entradas_salidas_contol_flujo/06_do_while.dart
+++ TEORIA/04_entradas_salidas_contol_flujo/06_do_while.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void main () {
-  
+  String continuar = 'y' ;
+
+  int contador = 0 ;
+
+  do {
+    contador++;
+    print(contador);
+    stdout.writeln('¿Desea continuar? (y/n)');
+    continuar = stdin.readLineSync()!;
+  } while (continuar == 'y');
 }
